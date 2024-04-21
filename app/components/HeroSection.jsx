1590a182fe2974a50281e4eec80e10e326b7508e
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-04-21 21:15:29 +0800

    Adding download cv and duration of type animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import gradPic from '/public/images/grad-pic.png';
+import gradPic from "/public/images/grad-pic.png";
 import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
@@ -11,19 +11,20 @@ const HeroSection = () => {
         <div className="col-span-12 text-center sm:col-span-7 place-self-center sm:text-left">
           <h1 className="mb-4 text-4xl font-extrabold sm:text-5xl lg:text-6xl">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-emerald-600">
-              Hello everyone! {" "} I&apos;m {" "}
+              Hello everyone! I am{" "}
             </span>
             <br />
-            <TypeAnimation className="text-gray-800 dark:text-white"
+            <TypeAnimation
+              className="text-gray-800 dark:text-white"
               sequence={[
                 "Jan Lawrence Madali",
-                1000,
+                2000,
                 "a Web Developer",
-                1000,
+                2000,
                 "a Web App Developer",
-                1000,
+                2000,
                 "a Gamer",
-                1000
+                2000,
               ]}
               wrapper="span"
               speed={50}
@@ -31,8 +32,8 @@ const HeroSection = () => {
             />
           </h1>
           <p className="mb-6 text-base text-gray-600 sm:text-lg lg:text-xl dark:text-white">
-            I&apos;m a full stack web developer based in Manila, Philippines. I&apos;m
-            passionate about building scalable and maintainable web
+            I&apos;m a full stack web developer based in Manila, Philippines.
+            I&apos;m passionate about building scalable and maintainable web
             applications.
           </p>
           <div>
@@ -40,9 +41,14 @@ const HeroSection = () => {
               Hire me
             </button>
             <button className="w-full p-1 mt-3 mr-4 text-white rounded-full bg-gradient-to-br from-lime-500 via-green-500 to-emerald-500 sm:w-fit hover:bg-slate-900">
-              <span className="block px-5 py-2 text-gray-900 rounded-full bg-white/90 hover:bg-white dark:bg-gray-950 dark:text-white dark:hover:bg-gray-900">
+              <a
+                download
+                target="_blank"
+                href="/files/JanLawrenceMadaliCV.pdf"
+                className="block px-5 py-2 text-gray-900 rounded-full bg-white/90 hover:bg-white dark:bg-gray-950 dark:text-white dark:hover:bg-gray-900"
+              >
                 Download CV
-              </span>
+              </a>
             </button>
           </div>
         </div>
