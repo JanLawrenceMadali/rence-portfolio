72a60e2d3c8256be10ff2c1a471db4d6025c58f5
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-04-22 21:42:34 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import AboutSection from "./components/AboutSection";
+import EmailSection from "./components/EmailSection";
+import FooterSection from "./components/FooterSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import ProjectSection from "./components/ProjectSection";
 
 export default function Home() {
   return (
@@ -9,7 +12,10 @@ export default function Home() {
       <div className="container px-12 py-4 mt-24">
         <HeroSection />
         <AboutSection />
+        <ProjectSection />
+        <EmailSection />
       </div>
+      <FooterSection />
     </main>
   );
 }
