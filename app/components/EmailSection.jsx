9d4ba9d677ee4ad254c0ed5a72081f3a15f9119b
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-04-22 21:41:50 +0800

    Adding require files for resend to work and adding email section content

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,132 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import GithubIcon from "/public/github-icon.svg";
+import LinkedinIcon from "/public/linkedin-icon.svg";
+
+const EmailSection = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+
+    if (response.status === 200) {
+      console.log("Message sent.");
+      setEmailSubmitted(true);
+    }
+  };
+
+  return (
+    <section
+      id="contact"
+      className="grid grid-cols-1 gap-4 py-24 my-12 md:grid-cols-2"
+    >
+      <div>
+        <h5 className="my-2 text-xl font-bold text-white">
+          Let&apos;s Connect!
+        </h5>
+        <p className="max-w-md mb-4 text-gray-50">
+          {" "}
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
+        </p>
+        <div className="flex flex-row gap-2 socials">
+          <Link href="https://github.com/JanLawrenceMadali" target="_blank">
+            <Image src={GithubIcon} alt="Github Icon" />
+          </Link>
+          <Link href="www.linkedin.com/in/jan-lawrence-madali" target="_blank">
+            <Image src={LinkedinIcon} alt="Linkedin Icon" />
+          </Link>
+        </div>
+      </div>
+      <div>
+        {emailSubmitted ? (
+          <p className="mt-2 text-sm text-green-500">
+            Email sent successfully!
+          </p>
+        ) : (
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-white"
+              >
+                Your email
+              </label>
+              <input
+                name="email"
+                type="email"
+                id="email"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="your_email@gmail.com"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="subject"
+                className="block mb-2 text-sm font-medium text-white"
+              >
+                Subject
+              </label>
+              <input
+                name="subject"
+                type="text"
+                id="subject"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Just saying hi"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="block mb-2 text-sm font-medium text-white"
+              >
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="message"
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Let's talk about..."
+              />
+            </div>
+            <button
+              type="submit" disabled
+              className="bg-green-500 cursor-not-allowed hover:bg-green-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            >
+              Send Message
+            </button>
+          </form>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
