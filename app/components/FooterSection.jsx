8c656657de6cd6142a211faa521bb44e164c0d52
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-04-22 21:41:57 +0800

    Create FooterSection.jsx

---
+++ app/components/FooterSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FooterSection = () => {
+  return (
+    <footer className="z-10 text-white border footer border-t-gray-700 border-l-transparent border-r-transparent">
+      <div className="container flex justify-between p-12">
+        <span className="font-mono font-bold uppercase">Dev.Rence</span>
+        <p className="text-slate-500"> © 2024 All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterSection;
