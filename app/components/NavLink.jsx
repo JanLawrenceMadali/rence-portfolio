a14df4fa34c83b14910ff59ddee49e5b58db3de1
JanLawrenceMadali <jan.lawrence.madali@gmail.com>
2024-04-22 21:42:05 +0800

    Update NavLink.jsx

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -5,7 +5,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-gray-800 rounded dark:text-gray-300 sm:text-xl md:p-0 hover:text-gray-900 dark:hover:text-white"
+      className="block py-2 pl-3 pr-4 text-gray-800 rounded dark:text-gray-400 sm:text-xl md:p-0 hover:text-gray-900 dark:hover:text-white"
     >
       {title}
     </Link>
